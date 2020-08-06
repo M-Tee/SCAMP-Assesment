59b4ea595107f9b9c1976536744a1e585956528f
mt <margarettom6@gmail.com>
2020-08-06 17:09:19 +0300

    Http Get function

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const db = mongoose.connect('mongodb://localhost/inventory');
 const Product = require('./Models/productModel');
 
 
+router.route('/products')
+.get((req, res) => {
+  Product.find((err, products) => {
+    if (err) {
+     return res.sendStatus(404);
+    }
+    return res.json(products)
+  });
+})
+.post();
+
 app.use('/shoppingmart', router);
 
 app.listen(port, () => {
