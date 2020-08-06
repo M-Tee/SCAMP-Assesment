f77074fb011dfd31894f580a2a134ca1623d2a24
mt <margarettom6@gmail.com>
2020-08-06 17:30:13 +0300

    Setup Body-parser and a POST function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require(`express`);
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8000;
@@ -7,6 +8,8 @@ const router = express.Router();
 const db = mongoose.connect('mongodb://localhost/inventory');
 const Product = require('./Models/productModel');
 
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 router.route('/products')
 .get((req, res) => {
@@ -17,7 +20,16 @@ router.route('/products')
     return res.json(products)
   });
 })
-.post();
+.post((req, res) => {
+  const product = new Product(req.body);
+
+  product.save((err) => {
+    if(err) {
+      return res.sendStatus(404);
+    }
+    return res.json(product);
+  })
+});
 
 app.use('/shoppingmart', router);
 
