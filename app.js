16662a4020f2092ff6f479dcebef343cd2ace702
mt <margarettom6@gmail.com>
2020-08-08 15:31:29 +0300

    Included userRoutes & userModel

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const db = mongoose.connect('mongodb://localhost/inventory');
 const Product = require('./Models/productModel');
 const router = require('./routes/productRoutes')(Product);
 
+const User = require('./Models/userModel');
+const userRouter = require('./routes/userRoutes')(User);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 app.use('/shoppingmart', router);
+app.use(userRouter);
 
 app.listen(port, () => {
   console.log(`listening on Port ${port}`);
