ab8d5b4c4996e4c41d1077dbbd15149f90a153f6
mt <margarettom6@gmail.com>
2020-08-08 15:28:54 +0300

    Created a UserModel

---
+++ Models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userModel = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+  }
+);
+
+module.exports = mongoose.model('User', userModel);
