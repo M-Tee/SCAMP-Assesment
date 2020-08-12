8fbf8045370207338d43121b78b3af564b9b4dda
mt <margarettom6@gmail.com>
2020-08-12 16:53:17 +0300

    User router code refactoring

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require(`express`);
 const bcrypt = require('bcrypt');
+// const controller = require('controllers/controller.js');
 
-function userRoutes(User) {
-  const router = express.Router();
+const router = express.Router();
+const User = require('../Models/userModel');
 
-  router.route('/users')
+router.route('/users')
     .get((req, res) => {
       User.find((err, users) => {
         if (err) {
@@ -54,13 +55,76 @@ function userRoutes(User) {
           return res.send(err);
         }
         if (await bcrypt.compare(req.body.password, user.password)) {
-          return res.send('Login sucessful')
+          // return res.send('Login sucessful')
+          return res.send(`Welcome back ${user.firstName}`)
         }
         return res.send('Wrong Password');
       })
-    })
+    });
 
-  return router;
-}
+// function userRoutes(User) {
+//   const router = express.Router();
 
-module.exports = userRoutes;
+//   router.route('/users')
+//     .get((req, res) => {
+//       User.find((err, users) => {
+//         if (err) {
+//           return res.sendStatus(404);
+//         }
+//         return res.json(users)
+//       });
+//     })
+//     .post(async (req, res) => {
+//       try {
+//         const salt = await bcrypt.genSalt();
+//         req.body.password = await bcrypt.hash(req.body.password, salt)
+
+//         const user = new User(req.body);
+
+//         user.save((err) => {
+//           if (err) {
+//             return res.send(err);
+//           }
+//           return res.json(user);
+//         })
+//       } catch {
+//         res.status(400).send()
+//       }
+//     });
+
+//     router.route('/users/:userId')
+//     .delete((req, res) => {
+//       User.findById(req.params.userId, (err, user) => {
+//         if (err) {
+//           return res.send(err);
+//         }
+//         user.remove((err) => {
+//           if (err) {
+//             return res.send(err);
+//           }
+//           return res.sendStatus(204);
+//         })
+//       })
+
+//     });
+
+//   router.route('/users/login')
+//     .post((req, res) => {
+//       User.findOne({ email: req.body.email }, async (err, user) => {
+//         if (err) {
+//           return res.send(err);
+//         }
+//         if (await bcrypt.compare(req.body.password, user.password)) {
+//           // return res.send('Login sucessful')
+//           return res.send(`Welcome back ${user.firstName}`)
+//         }
+//         return res.send('Wrong Password');
+//       })
+//     });
+
+//   return router;
+// }
+
+// module.exports = userRoutes;
+
+module.exports = router
