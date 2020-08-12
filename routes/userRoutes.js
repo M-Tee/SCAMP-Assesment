97e1756acfbb7dc15e29ea112b5af2956feac9a2
mt <margarettom6@gmail.com>
2020-08-12 17:11:01 +0300

    Moved logic to a controllers file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,66 +1,17 @@
 const express = require(`express`);
-const bcrypt = require('bcrypt');
-// const controller = require('controllers/controller.js');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
-const User = require('../Models/userModel');
 
 router.route('/users')
-    .get((req, res) => {
-      User.find((err, users) => {
-        if (err) {
-          return res.sendStatus(404);
-        }
-        return res.json(users)
-      });
-    })
-    .post(async (req, res) => {
-      try {
-        const salt = await bcrypt.genSalt();
-        req.body.password = await bcrypt.hash(req.body.password, salt)
+  .get(userController.getUsers)
+  .post(userController.postUser);
 
-        const user = new User(req.body);
+router.route('/users/:userId')
+  .delete(userController.deleteUser);
 
-        user.save((err) => {
-          if (err) {
-            return res.send(err);
-          }
-          return res.json(user);
-        })
-      } catch {
-        res.status(400).send()
-      }
-    });
-
-    router.route('/users/:userId')
-    .delete((req, res) => {
-      User.findById(req.params.userId, (err, user) => {
-        if (err) {
-          return res.send(err);
-        }
-        user.remove((err) => {
-          if (err) {
-            return res.send(err);
-          }
-          return res.sendStatus(204);
-        })
-      })
-
-    });
-
-  router.route('/users/login')
-    .post((req, res) => {
-      User.findOne({ email: req.body.email }, async (err, user) => {
-        if (err) {
-          return res.send(err);
-        }
-        if (await bcrypt.compare(req.body.password, user.password)) {
-          // return res.send('Login sucessful')
-          return res.send(`Welcome back ${user.firstName}`)
-        }
-        return res.send('Wrong Password');
-      })
-    });
+router.route('/users/login')
+  .post(userController.userLogin);
 
 // function userRoutes(User) {
 //   const router = express.Router();
