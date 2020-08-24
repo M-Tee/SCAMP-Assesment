5035bfcb28f101d2a22c007d41d4fce2fe342665
mt <margarettom6@gmail.com>
2020-08-24 13:23:05 +0300

    Installed and included morgan

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 const port = 8000;
@@ -9,12 +10,16 @@ const db = mongoose.connect('mongodb://localhost/inventory');
 const prodrouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
 
+app.use(morgan('dev'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 app.use(prodrouter);
 app.use(userRouter);
+
+
 
 app.listen(port, () => {
   console.log(`listening on Port ${port}`);
