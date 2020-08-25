9da0c8ac5afd6d0be07b4a39627b40f11d0835e2
mt <margarettom6@gmail.com>
2020-08-25 08:24:01 +0300

    App.js code Refactoring

--- app.js
+++ app.js
@@ -4,23 +4,19 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const db = mongoose.connect('mongodb://localhost/inventory');
 
 const prodrouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(morgan('dev'));
+app
+.use(morgan('dev'))
+.use(bodyParser.urlencoded({ extended: true }))
+.use(bodyParser.json())
+.use(prodrouter)
+.use(userRouter)
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-app.use(prodrouter);
-app.use(userRouter);
-
-
-
-app.listen(port, () => {
+.listen(port, () => {
   console.log(`listening on Port ${port}`);
 });
