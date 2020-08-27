f002f3c3d260c2136e2c6b5d7865bc75c2bf83d6
mt <margarettom6@gmail.com>
2020-08-27 13:49:43 +0300

    mongoDB atlas connection

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
+require('dotenv').config()
 const express = require(`express`);
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+
 const app = express();
 const port = process.env.PORT || 8000;
-const db = mongoose.connect('mongodb://localhost/inventory');
+const mongoDB = process.env.MONGODB_URI
+// const db = mongoose.connect('mongodb://localhost/inventory');
+mongoose
+  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 const prodrouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
 
 app
-.use(morgan('dev'))
-.use(bodyParser.urlencoded({ extended: true }))
-.use(bodyParser.json())
-.use(prodrouter)
-.use(userRouter)
+  .use(morgan('dev'))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(prodrouter)
+  .use(userRouter)
 
-.listen(port, () => {
-  console.log(`listening on Port ${port}`);
-});
+  .listen(port, () => {
+    console.log(`listening on Port ${port}`);
+  });
